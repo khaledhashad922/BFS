6980144d0c49a3e649ec836391dbced34183497d
KhaledHashad <krhashad@gmail.com>
2024-09-02 06:19:20 +0300

    kh

--- BFS.py
+++ BFS.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 
 class TicTacToe:
-    # khaled
+
     def __init__(self, root):
         self.root = root
         self.root.title("Tic Tac Toe")
@@ -22,14 +22,17 @@ class TicTacToe:
                                               command=lambda row=i, col=j: self.on_click(row, col))
                 self.buttons[i][j].grid(row=i, column=j)
 
+
     def check_win(self, player):
         for i in range(3):
             if all(self.board[i][j] == player for j in range(3)) or all(self.board[j][i] == player for j in range(3)):
                 return True
         return all(self.board[i][i] == player for i in range(3)) or all(self.board[i][2 - i] == player for i in range(3))
 
+
     def is_full(self):
         return all(cell != "" for row in self.board for cell in row)
+
 
     def make_computer_move(self):
         if self.is_full():
@@ -42,6 +45,7 @@ class TicTacToe:
                     self.buttons[row][col]['text'] = "O"
                     return
 
+    #Algo
     def on_click(self, row, col):
         if self.board[row][col] == "" and not self.check_win("X"):
             self.board[row][col] = "X"
